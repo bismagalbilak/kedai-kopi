a8a07104d5b5ff94fb5118d7da76b1a1ad6efbdf
bismaglbilak <bismaaezt2@gmail.com>
2024-04-28 21:53:27 +0700

    Latihan file

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Toggle Class active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu nya di klik //
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
